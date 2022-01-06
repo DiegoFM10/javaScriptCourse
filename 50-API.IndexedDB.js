ad7bfb97ad6038a747e8da103b3f7161d72cc3df
diegoFm10 <diego479@gmail.com>
2022-01-06 16:07:30 -0500

    JS course continuation

--- 50-API.IndexedDB.js
+++ 50-API.IndexedDB.js
@@ -16,6 +16,17 @@
             -Crear almacén de objetos con el método {createObjectStore()}
             -Escuchar los eventos de éxito y error con los métodos onsuccess() y onerror()
 
+        Contenido extra para buscar:
+            Método getAll() para obtener todos los registros de la base de datos
+            Método clear() para borrar objetos del almacen
+            Método deleteDatabase() para borrar la base de datos
+            Metodo onBlocked() para bloquear bases de datos en los cambios de version
+            Objeto IDBKeyRange para búsquedas en la base de datos
+            Método createIndex() para la creación de índices en la base de datos
+            Versionado de bases de datos
+            Libreria indexedDB
+                https://dexie.org/
+                
     Especificación oficial: https://developer.mozilla.org/es/docs/IndexedDB-840092-dup
 */
 
@@ -38,8 +49,8 @@ if (indexedDB && form)
         db = request.result
         console.log('Create', db)
         const objectStore = db.createObjectStore('tasks', {
-            // keyPath: 'taskTitle' //Es otra forma posible para agregar a la llave uno de los valores recopilados en el formulario
-            autoIncrement: true
+            keyPath: 'taskTitle' //Es otra forma posible para agregar a la llave uno de los valores recopilados en el formulario
+            // autoIncrement: true
         })
     }
 
@@ -55,6 +66,41 @@ if (indexedDB && form)
         readData()
     }
     
+    const getData = (key) => {
+        const transaction = db.transaction(['tasks'], 'readwrite')
+        const objectStore = transaction.objectStore('tasks')
+        const request = objectStore.get(key)
+
+        request.onsuccess = () => {
+            form.task.value = request.result.taskTitle
+            form.priority.value = request.result.taskPriority
+            form.button.dataset.action = 'update'
+            form.button.textContent = 'Update Task'
+        }
+    }
+
+    const updateData = (data) => {
+        const transaction = db.transaction(['tasks'], 'readwrite')
+        const objectStore = transaction.objectStore('tasks')
+        const request = objectStore.put(data)
+        request.onsuccess = () => {
+            form.button.dataset.action = 'add'
+            form.button.textContent = 'Add Task'
+            readData()
+        }
+        
+    }
+
+    const deleteData = (key) => {
+        const transaction = db.transaction(['tasks'], 'readwrite')
+        const objectStore = transaction.objectStore('tasks')
+        const request = objectStore.delete(key)
+        request.onsuccess = () => {
+            readData()
+        }
+        
+    }
+
     const readData = (data) => {
         const transaction = db.transaction(['tasks'], 'readonly')
         const objectStore = transaction.objectStore('tasks')
@@ -73,6 +119,19 @@ if (indexedDB && form)
                 taskPriority.textContent = cursor.value.taskPriority
                 fragment.appendChild(taskPriority)
 
+                const taskUpdate = document.createElement('BUTTON')
+                taskUpdate.dataset.type = 'update'
+                taskUpdate.dataset.key = cursor.key
+                taskUpdate.textContent = 'Update'
+                fragment.appendChild(taskUpdate)
+
+                const taskDelete = document.createElement('BUTTON')
+                taskDelete.dataset.type = 'delete'
+                taskDelete.dataset.key = cursor.key
+                taskDelete.textContent = 'Delete'
+                fragment.appendChild(taskDelete)
+                
+
                 cursor.continue()
             }
             else
@@ -89,7 +148,28 @@ if (indexedDB && form)
             taskTitle : e.target.task.value,
             taskPriority : e.target.priority.value
         }
-        console.log(data)
-        addData(data)
+
+        if (e.target.button.dataset.action == 'add') 
+        {
+            addData(data)
+        }
+        else if(e.target.button.dataset.action == 'update')
+        {
+            updateData(data)
+        }
+
+        form.reset()
+
+    })
+
+    tasks.addEventListener('click', (e) => {
+        if (e.target.dataset.type == 'update')
+        {
+            getData(e.target.dataset.key)
+        }
+        else if (e.target.dataset.type == 'delete')
+        {
+            deleteData(e.target.dataset.key)
+        }
     })
 }
